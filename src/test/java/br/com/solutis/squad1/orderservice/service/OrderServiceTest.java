dd23967ec97342b196610233750e6920ad83b8ff
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-11-09 11:10:10 -0300

    feat: add service tests

--- src/test/java/br/com/solutis/squad1/orderservice/service/OrderServiceTest.java
+++ src/test/java/br/com/solutis/squad1/orderservice/service/OrderServiceTest.java
@@ -1,4 +1,4 @@
-package br.com.solutis.squad1.orderservice;
+package br.com.solutis.squad1.orderservice.service;
 
 import br.com.solutis.squad1.orderservice.dto.order.OrderResponseDto;
 import br.com.solutis.squad1.orderservice.mapper.OrderMapper;
@@ -9,7 +9,10 @@ import br.com.solutis.squad1.orderservice.model.repository.OrderRepository;
 import br.com.solutis.squad1.orderservice.model.repository.OrderRepositoryCustom;
 import br.com.solutis.squad1.orderservice.service.OrderService;
 import br.com.solutis.squad1.orderservice.service.ProductService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.NoResultException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,10 +23,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class OrderServiceTest {
@@ -59,5 +65,24 @@ public class OrderServiceTest {
         Page<OrderResponseDto> result = orderService.findAll(pageable);
         assertEquals(0, result.getContent().size());
         verify(orderRepositoryCustom, times(1)).findAll(pageable);
+    }
+
+    @Test
+    void whenFindByIdIsCalledThenReturnOrder() {
+        Long id = 1L;
+        Order order = new Order();
+        when(orderRepositoryCustom.findById(id)).thenReturn(order);
+        when(orderService.findProductsByOrderId(order.getId())).thenReturn(new ArrayList<>());
+        OrderResponseDto result = orderService.findById(id);
+        assertNotNull(result);
+        verify(orderRepositoryCustom, times(1)).findById(id);
+    }
+
+    @Test
+    void whenFindByIdIsCalledThenThrowEntityNotFoundException() {
+        Long id = 1L;
+        when(orderRepositoryCustom.findById(id)).thenThrow(new NoResultException());
+        assertThrows(EntityNotFoundException.class, () -> orderService.findById(id));
+        verify(orderRepositoryCustom, times(1)).findById(id);
     }
 }
